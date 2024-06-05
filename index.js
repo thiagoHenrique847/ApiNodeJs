3705b399a015eceb2cfb9d477de6bb838974b4b6
43223423 <thiagocontato1232@gmail.com>
2024-06-05 16:57:29 -0300

    Update Chart

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
-const uri = process.env.MONGODB_URI;
-
+// const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://thiagocontato1232:MMmLvwm1aLHeV61u@cluster0.ob27bb5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -15,8 +15,6 @@ const client = new MongoClient(uri, {
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  
-
   try {
     res.status(200).send("Node js");
   } catch (error) {
@@ -39,7 +37,7 @@ app.get("/chart-data-pequena", async (req, res) => {
     res.status(200).send(collection);
   } catch (error) {
     res.status(500).send(error);
-  }finally {
+  } finally {
     await client.close();
   }
 });
@@ -60,7 +58,7 @@ app.get("/chart-data-media", async (req, res) => {
     res.status(200).send(collection);
   } catch (error) {
     res.status(500).send(error);
-  }finally {
+  } finally {
     await client.close();
   }
 });
@@ -81,20 +79,22 @@ app.get("/chart-data-grande", async (req, res) => {
     res.status(200).send(collection);
   } catch (error) {
     res.status(500).send(error);
-  }finally {
+  } finally {
     await client.close();
   }
 });
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3300;
+const PORT = process.env.PORT || 3300;
 
-
-app.listen({
-  host:"0.0.0.0",
-  port:PORT
-}, function(){
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.listen(
+  {
+    host: "0.0.0.0",
+    port: PORT,
+  },
+  function () {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  }
+);
 
 async function Delete() {
   await client.connect();
